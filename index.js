7c89a8a92d1926041c42fad655839d754653fddd
ChukwukaEjinkonye-jpg <chukwukaejinkonye@gmail.com>
2023-03-27 15:54:53 -0400

    new files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Pokemon from "./pokemon.js";
+
 const listCards = document.querySelector('#list-cards'),
 pokeSprite = document.querySelector("#pokeSprite"),
 logoSprite = document.querySelector('#logo-sprite'),
@@ -94,53 +96,7 @@ deleteButton.addEventListener("click" , () => {
     console.log(player1.party)
 })
 
-class Pokemon{
-    #name
-    #atk
-    #def
-    #hp
-    #moves
-    constructor(name , atk , def , hp){
-        this.#name = name
-        this.#atk = atk
-        this.#def = def
-        this.#hp = hp
-        this.#moves = []
-    }
 
-    get name(){
-        return this.#name
-    }
-    get atk(){
-        return this.#atk
-    }
-    get def(){
-        return this.#def
-    }
-    get hp(){
-        return this.#hp
-    }
-    get moves(){
-        return this.moves
-    }
-
-    set moves(value){
-        this.#moves.push(value) // Needs further implementation
-    } 
-    set name(value){
-        this.#name = value
-    }
-    set atk(value){
-        this.#atk = value
-    }
-    set def(value){
-        this.#def = value
-    }
-    set hp(value){
-        this.#hp = value
-    }
-
-}
 
 
 
---
+++ pokemon.js
@@ -0,0 +1,47 @@
+export default class Pokemon{
+    #name
+    #atk
+    #def
+    #hp
+    #moves
+    constructor(name , atk , def , hp){
+        this.#name = name
+        this.#atk = atk
+        this.#def = def
+        this.#hp = hp
+        this.#moves = []
+    }
+
+    get name(){
+        return this.#name
+    }
+    get atk(){
+        return this.#atk
+    }
+    get def(){
+        return this.#def
+    }
+    get hp(){
+        return this.#hp
+    }
+    get moves(){
+        return this.moves
+    }
+
+    set moves(value){
+        this.#moves.push(value) // Needs further implementation
+    } 
+    set name(value){
+        this.#name = value
+    }
+    set atk(value){
+        this.#atk = value
+    }
+    set def(value){
+        this.#def = value
+    }
+    set hp(value){
+        this.#hp = value
+    }
+
+}
